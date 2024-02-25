dbcb2e6d94c96bfb6cd6737fd2985e24fbd73124
mahadiinsta <mahadi.hpu@gmail.com>
2024-02-25 13:57:22 +0600

    responsive added

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -14,7 +14,6 @@ export default function CoursePage({ playlist, userId }) {
     try {
       const updatedVideos = [...playlist.videos];
       updatedVideos[index].watched = !updatedVideos[index].watched;
-      // setVideos(updatedVideos);
 
       // Update video completion status in the database
       await axios.put(
@@ -30,48 +29,49 @@ export default function CoursePage({ playlist, userId }) {
     if (playlist.videos.length > 0) {
       setSelectedVideo(playlist.videos[0]);
     }
-  }, [playlist]); 
+  }, [playlist]);
 
   return (
-    <div className="flex">
-      <div className="w-2/4">
+    <div className="flex flex-wrap modal-container bg-white p-4 md:p-8 rounded-lg shadow-lg items-center">
+      <div className="w-full md:w-1/2 items-center">
+      <p className="text-xl font-bold mb-2">{selectedVideo.title}</p>
         {selectedVideo && (
           <ReactPlayer
             url={`https://www.youtube.com/watch?v=${selectedVideo.videoId}`}
             controls={true}
-            width="500px"
+            className="react-player"
+            width="100%"
+            // height="100%"
           />
         )}
       </div>
-      <div className="w-2/4">
-        <div>
-          <p className="text-xl font-bold mb-2">{playlist.title}</p>
-          {playlist.videos.map((video, index) => (
-            <div
-              key={index}
-              className={`flex items-center cursor-pointer mb-2 ${
-                video.watched ? "bg-gray-200" : ""
-              }`}
-              onClick={() => handleVideoSelect(video.videoId)}
-            >
-              <input
-                type="checkbox"
-                checked={video.watched}
-                onChange={() => handleCheckboxChange(index)}
-                className="mr-2"
-              />
-              <img
-                src={video.thumbnail}
-                alt={video.title}
-                className="w-12 h-12 mr-2"
-              />
-              <div>
-                <p className="text-xs font-bold">{video.title}</p>
-                <p>{video.description}</p>
-              </div>
+      <div className="w-full md:w-1/2 p-2">
+        <p className="text-xl font-bold mb-2">{playlist.title}</p>
+        {playlist.videos.map((video, index) => (
+          <div
+            key={index}
+            className={`flex items-center cursor-pointer mb-2 p-2 ${
+              video.watched ? "bg-gray-200" : ""
+            }`}
+            onClick={() => handleVideoSelect(video.videoId)}
+          >
+            <input
+              type="checkbox"
+              checked={video.watched}
+              onChange={() => handleCheckboxChange(index)}
+              className="mr-2"
+            />
+            <img
+              src={video.thumbnail}
+              alt={video.title}
+              className="w-12 h-12 mr-2"
+            />
+            <div className="flex-grow">
+              <p className="text-xs font-bold">{video.title}</p>
+              <p className="text-xs">{video.description}</p>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
